cb92b57514206c92180dfb4e2ece94a7078f3f82
lebeaugs <ianhafiz9999@gmail.com>
2026-03-28 01:35:59 +0100

    feat(backend): gestion de la déconnexion et affichage exclusif des utilisateurs actifs

--- enregistrer.php
+++ enregistrer.php
@@ -6,6 +6,7 @@ $message = $_POST['message'];
 
 $stmt = $conn->prepare("INSERT INTO messages (auteur, contenu) VALUES (?, ?)");
 $stmt->bind_param("ss", $auteur, $message);
+$conn->query("UPDATE users SET last_active = NOW() WHERE pseudo = '$auteur'");
 
 $stmt->execute();
 
--- users.php
+++ users.php
@@ -1,7 +1,7 @@
 <?php
 require("config.php");
 
-$sql = "SELECT pseudo FROM users";
+$sql = "SELECT pseudo FROM users WHERE last_active > NOW() - INTERVAL 10 SECOND";
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
