51052411fbbf6d591773129604f9c93e195ff5f9
lebeaugs <ianhafiz9999@gmail.com>
2026-04-02 12:46:07 +0200

    style: amélioration de l'affichage de la liste des utilisateurs actifs

--- users.php
+++ users.php
@@ -6,7 +6,7 @@ $sql = "SELECT pseudo FROM users WHERE last_active > NOW() - INTERVAL 10 SECOND"
 $result = $conn->query($sql);
 
 while ($row = $result->fetch_assoc()) {
-    echo "<p>" . $row['pseudo'] . "</p>";
+    echo "<div class='user'>" . $row['pseudo'] . "</div>";
 }
 
 $conn->close();
