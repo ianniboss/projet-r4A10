831d037de364b30691131657e0e50ebb9965e0fc
lebeaugs <ianhafiz9999@gmail.com>
2026-04-03 11:13:53 +0200

    feat(backend): récupération des messages filtrés par salle de chat

--- recuperer.php
+++ recuperer.php
@@ -1,11 +1,15 @@
 <?php
 require("config.php");
 
-$last_id = isset($_GET['last_id']) ? intval($_GET['last_id']) : 0;
+$salle = $_GET['salle'];
+$last_id = intval($_GET['last_id']);
 
-$sql = "SELECT * FROM messages WHERE id > ? ORDER BY id ASC";
+$sql = "SELECT * FROM messages 
+        WHERE id > ? AND salle = ? 
+        ORDER BY id ASC";
+
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $last_id);
+$stmt->bind_param("is", $last_id, $salle);
 $stmt->execute();
 
 $result = $stmt->get_result();
